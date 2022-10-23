d7598b07461d0b8a5b0de685f99d63627c007e08
minomi <minomi@minomiui-MacBookPro.local>
2022-10-23 22:38:29 +0900

    Feat: 프로젝트 화면 기본 기능 추가 #20

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -1,11 +1,110 @@
 export default function Projects() {
   return (
-    <section class="flex-grow text-gray-600 body-font">
-      <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-        <div class="lg:flex-grow md:w-1/2 flex flex-col md:items-start md:text-left items-center text-center">
-          <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-            Projects
-          </h1>
+    <section className="flex-grow text-gray-600 body-font">
+      <div className="container mx-auto flex px-5 py-5 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 flex flex-col md:items-start md:text-left items-center text-center">
+          <div className="container px-5 py-5 mx-auto">
+            <div className="flex flex-col text-center w-full mb-20">
+              <h1 className="text-2xl font-medium title-font mb-4 text-gray-900 tracking-widest">
+                사이드 프로젝트
+              </h1>
+              <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+                유투브나 구글링을 통해 알게된 JS라이브러리나 기술의 알아보고
+                Getting Start를 따라하여 해당 기술의 베이스를 익힙니다. <br />
+                이를 활용하여 개인적인 사이드 프로젝트를 진행해 Growth mindset
+                바탕의 개인 성장을 목표로 합니다. <br />
+              </p>
+            </div>
+            <div className="flex flex-wrap -m-4">
+              <div className="p-4 lg:w-1/2">
+                <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+                  <img
+                    alt="team"
+                    className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4"
+                    src="https://dummyimage.com/200x200"
+                  />
+                  <div className="flex-grow sm:pl-8">
+                    <h2 className="title-font font-medium text-lg text-gray-900">
+                      자기 PR 웹페이지
+                    </h2>
+                    <h3 className="text-gray-500 mb-3">2022.10 ~ 2022.11</h3>
+                    <p className="mb-4">
+                      자기 PR을 목표로 내가 활용해본 기술과 이를 바탕으로
+                      만들어본 사이드 프로젝트를 보여주는 프로젝트입니다.
+                    </p>
+                    <a class="text-indigo-500 inline-flex items-center cursor-pointer">
+                      바로가기
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        class="w-4 h-4 ml-2"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                      </svg>
+                    </a>
+                    <div className="mt-3 cursor-pointer">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                      </svg>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="p-4 lg:w-1/2">
+                <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+                  <img
+                    alt="team"
+                    className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4"
+                    src="https://dummyimage.com/200x200"
+                  />
+                  <div className="flex-grow sm:pl-8">
+                    <h2 className="title-font font-medium text-lg text-gray-900">
+                      Notna Project
+                    </h2>
+                    <h3 className="text-gray-500 mb-3">2022.5 ~ 진행중</h3>
+                    <p className="mb-4">
+                      저출산 문제로 많은 이슈가 나타나고 있고 통계청 데이터를
+                      바탕으로 누구나 쉽게 저출산 문제에 관심을 가질 수 있도록
+                      보여주는 프로젝트입니다.
+                    </p>
+                    <a className="text-indigo-500 inline-flex items-center cursor-pointer">
+                      바로가기
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        class="w-4 h-4 ml-2"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                      </svg>
+                    </a>
+                    <div className="mt-3 cursor-pointer">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                      </svg>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
